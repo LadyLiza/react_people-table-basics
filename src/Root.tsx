47ec805d154e5a3f8a90c3d24ce30437d8d7785d
LadyLiza <elizabet132010@gmail.com>
2025-05-28 21:24:40 +0200

    split components into separate folders and add missing pages

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,12 +1,14 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { PeoplePage } from './components/PeoplePage/PeoplePage';
+import { HomePage } from './components/HomePage/HomePage';
+import { NotFoundPage } from './components/NotFoundPage/NotFoundPage';
 
 export const Root = () => (
   <Routes>
     <Route path="/home" element={<Navigate to="/" replace />} />
-    <Route path="/" element={<h1 className="title">Home Page</h1>} />
+    <Route path="/" element={<HomePage />} />
     <Route path="/people" element={<PeoplePage />} />
     <Route path="/people/:personSlug" element={<PeoplePage />} />
-    <Route path="*" element={<h1 className="title">Page not found</h1>} />
+    <Route path="*" element={<NotFoundPage />} />
   </Routes>
 );
--- src/components/PeoplePage/PeoplePage.tsx
+++ src/components/PeoplePage/PeoplePage.tsx
@@ -2,17 +2,12 @@ import { useEffect, useState } from 'react';
 import { getPeople } from '../../api';
 import { Loader } from '../Loader/Loader';
 import { Person } from '../../types';
-import { Link, useParams } from 'react-router-dom';
+import { PeopleTable } from '../PeopleTable/PeopleTable';
 
 export const PeoplePage = () => {
   const [people, setPeople] = useState<Person[]>([]);
   const [loader, setLoader] = useState(false);
   const [error, setError] = useState(false);
-
-  const { personSlug } = useParams();
-
-  const findPerson = (name: string | null) =>
-    people.find(person => person.name === name);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -35,6 +30,7 @@ export const PeoplePage = () => {
   return (
     <>
       <h1 className="title">People Page</h1>
+
       <div className="block">
         <div className="box table-container">
           {loader && <Loader />}
@@ -46,79 +42,8 @@ export const PeoplePage = () => {
           {people.length === 0 && !loader && !error && (
             <p data-cy="noPeopleMessage">There are no people on the server</p>
           )}
-          {!loader && (
-            <table
-              data-cy="peopleTable"
-              className="table is-striped is-hoverable is-narrow is-fullwidth"
-            >
-              <thead>
-                <tr>
-                  <th>Name</th>
-                  <th>Sex</th>
-                  <th>Born</th>
-                  <th>Died</th>
-                  <th>Mother</th>
-                  <th>Father</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                {people.map(person => (
-                  <tr
-                    data-cy="person"
-                    key={person.name}
-                    className={
-                      person.slug === personSlug ? 'has-background-warning' : ''
-                    }
-                  >
-                    <td>
-                      <Link
-                        to={`/people/${person.slug}`}
-                        className={person.sex === 'f' ? 'has-text-danger' : ''}
-                      >
-                        {person.name}
-                      </Link>
-                    </td>
-
-                    <td>{person.sex}</td>
-                    <td>{person.born}</td>
-                    <td>{person.died}</td>
-                    <td
-                      className={
-                        findPerson(person.motherName) ? 'has-text-danger' : ''
-                      }
-                    >
-                      {findPerson(person.motherName) ? (
-                        <Link
-                          className="has-text-danger"
-                          to={`/people/${findPerson(person.motherName)?.slug}`}
-                        >
-                          {person.motherName}
-                        </Link>
-                      ) : person.motherName ? (
-                        person.motherName
-                      ) : (
-                        '-'
-                      )}
-                    </td>
-
-                    <td>
-                      {findPerson(person.fatherName) ? (
-                        <Link
-                          to={`/people/${findPerson(person.fatherName)?.slug}`}
-                        >
-                          {person.fatherName}
-                        </Link>
-                      ) : person.fatherName ? (
-                        person.fatherName
-                      ) : (
-                        '-'
-                      )}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+          {!loader && !error && people.length > 0 && (
+            <PeopleTable people={people} />
           )}
         </div>
       </div>
